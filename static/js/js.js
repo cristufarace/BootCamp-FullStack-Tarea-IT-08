e522cbc3bc1d17125dc4cb3bd8f5d0f7508ee472
Cristu Farace <cristianfarace@gmail.com>
2022-11-11 14:34:39 -0300

    ok

--- static/js/js.js
+++ static/js/js.js
@@ -1,8 +1,8 @@
-// variables 
+// variables
 let boton = document.getElementById("reestablecer_btn");
 let boton2 = document.getElementById("estilos_btn");
 let btnRecordar = document.getElementById("recordar");
-
+let legend = document.getElementsByTagName("legend");
 
 // boton reestablecer
 boton.addEventListener("click", (e) => {
@@ -34,69 +34,103 @@ boton2.addEventListener("click", () => {
 
 // no cambiar fondo de legend, aplicar blur, y aplicar focus si el estilo actual es SIN ESTILOS CSS
 
-  let estilos = document.getElementsByTagName("link")[0].href
-  let sinCss = estilos.includes("sin")
+window.addEventListener("click", () => {
+  let estilos = document.getElementsByTagName("link")[0].href;
+  let sinCss = estilos.includes("sin");
+  console.log(estilos);
   console.log(sinCss);
 
-  // si no tiene una hoja de estilos css no hgas nada
-  if (sinCss == false){
-  }
-  // si tiene alguna hoja de estilos css entonces........
-  else {
-    
+  // si entra al if es porqe no hay estilos
+  if (sinCss === true) {
+    legend[0].style.background = "";
+    legend[1].style.background = "";
+    legend[2].style.background = "";
 
-// cambiando fondo de legend al desencaddenarse el foco
-const inputs = document.getElementsByTagName("input");
-// console.log(inputs);
-
-// efecto focus de los inputs
-for (let inp = 0; inp < inputs.length; inp++) {
-  let legend = document.getElementsByTagName("legend");
-  inputs[inp].addEventListener("focus", () => {
-    if (inp <= 6) {
-      legend[0].style.background = "#2d3c39";
-    } else if ((inp == 10) || (inp ==11)) {
-      legend[1].style.background = "#2d3c39";
-    } else {
-      legend[2].style.background = "#2d3c39";
+    const inputs = document.getElementsByTagName("input");
+    for (let inp = 0; inp < inputs.length; inp++) {
+      inputs[inp].addEventListener("focus", () => {
+        legend[0].style.background = "";
+        legend[1].style.background = "";
+        legend[2].style.background = "";
+      });
     }
-  });
-}
+    for (let inp = 0; inp < inputs.length; inp++) {
+      inputs[inp].addEventListener("blur", () => {
+        legend[0].style.background = "";
+        legend[1].style.background = "";
+        legend[2].style.background = "";
+      });
+    }
 
-// efecto blur de los inputs
-for (let inp = 0; inp < inputs.length; inp++) {
-  inputs[inp].addEventListener("blur", () => {
-    let legend = document.getElementsByTagName("legend");
-    legend[0].style.background = "#4b6963";
-    legend[1].style.background = "#4b6963";
-    legend[2].style.background = "#4b6963";
-  });
-}
-
-// efecto focus textarea
-const textarea = document.getElementsByTagName("textArea");
-for (let inp = 0; inp < textarea.length; inp++) {
-  textarea[inp].addEventListener("focus", () => {
-    console.log("se hizo foco");
-    let legend = document.getElementsByTagName("legend");
-    legend[0].style.background = "#2d3c39";
-  });
-}
-
-// efecto blur  textarea
-for (let inp = 0; inp < textarea.length; inp++) {
-  textarea[inp].addEventListener("blur", () => {
-    let legend = document.getElementsByTagName("legend");
-    legend[0].style.background = "#4b6963";
-  });
-}
+      // efecto focus textarea
+      const textarea = document.getElementsByTagName("textArea");
+      for (let inp = 0; inp < textarea.length; inp++) {
+        textarea[inp].addEventListener("focus", () => {
+          console.log("se hizo foco");
+          let legend = document.getElementsByTagName("legend");
+          legend[0].style.background = "";
+        });
+      }
   
+      // efecto blur  textarea
+      for (let inp = 0; inp < textarea.length; inp++) {
+        textarea[inp].addEventListener("blur", () => {
+          let legend = document.getElementsByTagName("legend");
+          legend[0].style.background = "";
+        });
+      }
+    
+    
+  }
+  // si entra al else es porque hay estilos, ya sea futuro, retro o originales
+  else {
+    // cambiando fondo de legend al desencaddenarse el foco
+    // efecto focus de los inputs
+    const inputs = document.getElementsByTagName("input");
+    for (let inp = 0; inp < inputs.length; inp++) {
+      let legend = document.getElementsByTagName("legend");
+      inputs[inp].addEventListener("focus", () => {
+        if (inp <= 6) {
+          legend[0].style.background = "#2d3c39";
+        } else if (inp == 10 || inp == 11) {
+          legend[1].style.background = "#2d3c39";
+        } else {
+          legend[2].style.background = "#2d3c39";
+        }
+      });
+    }
 
+    // efecto blur de los inputs
+    for (let inp = 0; inp < inputs.length; inp++) {
+      inputs[inp].addEventListener("blur", () => {
+        let legend = document.getElementsByTagName("legend");
+        legend[0].style.background = "#4b6963";
+        legend[1].style.background = "#4b6963";
+        legend[2].style.background = "#4b6963";
+      });
+    }
 
-}
+    // efecto focus textarea
+    const textarea = document.getElementsByTagName("textArea");
+    for (let inp = 0; inp < textarea.length; inp++) {
+      textarea[inp].addEventListener("focus", () => {
+        console.log("se hizo foco");
+        let legend = document.getElementsByTagName("legend");
+        legend[0].style.background = "#2d3c39";
+      });
+    }
 
+    // efecto blur  textarea
+    for (let inp = 0; inp < textarea.length; inp++) {
+      textarea[inp].addEventListener("blur", () => {
+        let legend = document.getElementsByTagName("legend");
+        legend[0].style.background = "#4b6963";
+      });
+    }
+  }
 
-
+  console.log("se hizo clic");
+});
 
 //-----------------------------------------------Bonus 1 estilos aleatorios
 let aleatorios = document.getElementById("aleatorio_btn");
@@ -109,47 +143,46 @@ aleatorios.addEventListener("click", () => {
     estilos.setAttribute("href", "./static/css/futuro.css");
   } else if ((numero > 25) & (numero < 50)) {
     estilos.setAttribute("href", "./static/css/retro.css");
-  }  else if ((numero > 50) & (numero < 75)) {
+  } else if ((numero > 50) & (numero < 75)) {
     estilos.setAttribute("href", "./static/css/estilos.css");
-  }
-  else {
+  } else {
     estilos.setAttribute("href", "./static/css/sin.css");
   }
-  
 });
 
-
-
 //-----------------------------------------------Bonus 2 guardo en local----------------------------
-btnRecordar.addEventListener("click",()=> {
+btnRecordar.addEventListener("click", () => {
   // si entra en este else es poruqe el boton NO esta hundido y guarda en local estilos para usarse desp
   console.log("Guardo estilos en local");
   // obtengo el estilo actual
   let estilos = document.getElementsByTagName("link")[0].href;
   // guardo en local el estilo actual
-  localStorage.setItem("estiloActual", JSON.stringify(estilos))
+  localStorage.setItem("estiloActual", JSON.stringify(estilos));
   // cuando se hace clic sobre el botn cambia la opacidad
-  btnRecordar.style.opacity = 0.4
-  console.log("Se guardara en Local el estilo: " + JSON.parse(localStorage.getItem("estiloActual"))) 
-}
-
-)
+  btnRecordar.style.opacity = 0.4;
+  console.log(
+    "Se guardara en Local el estilo: " +
+      JSON.parse(localStorage.getItem("estiloActual"))
+  );
+});
 
 // cada vez que carga la aplicacion va a buscar los estilos al local storage
 window.addEventListener("DOMContentLoaded", () => {
-// hago referencia a la etiqueta link
-let estilos = document.getElementsByTagName("link")[0];
-// obtengo el estilo que del local storage
-let recuperoEstilos = JSON.parse(localStorage.getItem("estiloActual")) ||  estilos.setAttribute("href", "./static/css/estilos.css");
-// le seteo a link el estilo que recupere del localstorage
-estilos.setAttribute("href",recuperoEstilos);
-// le agrego opacidad al boton recordar
-btnRecordar.style.opacity= 0.4
-console.log("Window on load. Cargando estilo recordado desde localSt: " + JSON.parse(localStorage.getItem("estiloActual"))); 
-})
-
-
-
+  // hago referencia a la etiqueta link
+  let estilos = document.getElementsByTagName("link")[0];
+  // obtengo el estilo que del local storage
+  let recuperoEstilos =
+    JSON.parse(localStorage.getItem("estiloActual")) ||
+    estilos.setAttribute("href", "./static/css/estilos.css");
+  // le seteo a link el estilo que recupere del localstorage
+  estilos.setAttribute("href", recuperoEstilos);
+  // le agrego opacidad al boton recordar
+  btnRecordar.style.opacity = 0.4;
+  console.log(
+    "Window on load. Cargando estilo recordado desde localSt: " +
+      JSON.parse(localStorage.getItem("estiloActual"))
+  );
+});
 
 //-----------------------------------------------Bonus 3 select
 let bonus3 = document.getElementById("Estilos-Bonus-3");
@@ -168,7 +201,7 @@ bonus3.addEventListener("click", (e) => {
 
 //-----------------------------------------------Bonus 4 select
 let bonus4 = document.querySelectorAll(".estilos-bonus-4");
-bonus4.forEach((e)=> {
+bonus4.forEach((e) => {
   e.addEventListener("click", (e) => {
     let estilos = document.getElementsByTagName("link")[0];
     if (e.target.value == "Original") {
@@ -181,10 +214,4 @@ bonus4.forEach((e)=> {
       estilos.setAttribute("href", "./static/css/sin.css");
     }
   });
-
-})
-
-
-
-
-
+});
